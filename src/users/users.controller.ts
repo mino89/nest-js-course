bf11b985fce525d2d2e31ccfda75e2c5ff1ac41d
mino89 <pierdomenicoreitano@gmail.com>
2023-05-07 22:26:10 +0200

    feat: implemented auth flow`

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
-import { Body, ClassSerializerInterceptor, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
-import { Serialize, SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 
 @Controller('auth')
@@ -13,38 +16,61 @@ export class UsersController {
   constructor(
     private usersService: UsersService,
     private authService: AuthService) { }
+  
+
+
+  // @Get('/whoami')
+  // whoAmI(@Session() session : any){
+  //   return this.usersService.findOne(session.userId)
+  // }
+
+  @Get('/whoami')
+  @UseGuards(AuthGuard)
+  whoAmI(@CurrentUser() user: User){
+    console.log(user)
+    return user
+  }
+
+  @Post('/signout')
+  signOut(@Session() session: any){
+    session.userId = null
+  }
   @Post('/signup')
-   createUser(@Body() body: CreateUserDto) {
-   return this.authService.signup(body.email, body.password)
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password)
+    session.userId = user.id
+    return user
   }
 
   @Post('/signin')
-  signin(@Body() body: CreateUserDto){
-    return this.authService.signIn(body.email,body.password)
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signIn(body.email, body.password)
+    session.userId = user.id
+    return user
   }
 
 
   @Get(`/:id`)
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id))
-    if (!user){
+    if (!user) {
       throw new NotFoundException('No user found!')
     }
     return user
   }
 
   @Get()
-  findAllUsers(@Query('email') email: string){
+  findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email)
   }
 
   @Delete(`/:id`)
-  removeUser(@Param('id') id: string){
+  removeUser(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id))
   }
-  
+
   @Patch(`/:id`)
-  updateUser(@Param('id') id:string, @Body() body: UpdateUserDto){
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
     return this.usersService.update(parseInt(id), body)
   }
 }
