5a7527ccac3c05dfee5e5066e6b7b70c7b88f525
mino89 <pierdomenicoreitano@gmail.com>
2023-05-16 21:09:34 +0200

    feat: implemented report creation

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,14 +1,20 @@
-import { AfterInsert, AfterRemove, AfterUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Report } from "src/reports/report.entity";
+import { AfterInsert, AfterRemove, AfterUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+  
   @Column()
   email: string;
+  
   @Column()
-
   password: string;
+
+  @OneToMany(()=> Report, (report)=> report.user)
+  reports: Report[]
+
   @AfterInsert()
   logInsert(){
     console.log(`inserted user with id`, this.id)
